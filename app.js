092c661e8bf0b155fb9148bb4fcb02342232fe4f
nadktk <nadezhda.katkova@gmail.com>
2019-10-02 10:15:42 +0300

    Express initialization

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// server will be here'
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from Server!'));
+
+const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server is running on port ${port}`));
