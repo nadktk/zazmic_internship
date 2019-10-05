ac09aa15ca10ee18a63d2e610d8d73df11886f38
nadktk <nadezhda.katkova@gmail.com>
2019-10-05 00:01:19 +0000

    serv fixes

--- utils/error-messages.js
+++ utils/error-messages.js
@@ -2,6 +2,6 @@ module.exports = {
   CONNECT_ERR: 'Database connection error',
   USERID_ERR: 'Wrong user ID',
   USERDATA_ERR: 'Wrong user data',
-  BLOGID_ERR: 'Wrong record data',
+  BLOGID_ERR: 'Wrong record ID',
   BLOGDATA_ERR: 'Wrong record data',
 };
